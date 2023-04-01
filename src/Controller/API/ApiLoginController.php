f4dafed58611600653a5c02a47fb30c96417ce2f
Thomas905 <thomasclarisse03@gmail.com>
2023-04-01 10:37:16 +0200

    login api

---
+++ src/Controller/API/ApiLoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\API;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class ApiLoginController extends AbstractController
+{
+    private JWTTokenManagerInterface $jwtManager;
+
+    public function __construct(JWTTokenManagerInterface $jwtManager)
+    {
+        $this->jwtManager = $jwtManager;
+    }
+
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): Response
+    {
+        if (null === $user) {
+             return $this->json([
+                 'message' => 'missing credentials',
+             ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'user' => $user->getId(),
+            'token' => $this->jwtManager->create($user),
+        ]);
+    }
+}
